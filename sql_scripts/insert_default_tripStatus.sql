b3bebd1aaf7097b92ea040b5c917ded20485cbca
Yoann P <yoann.pommier777@gmail.com>
2019-10-24 13:29:31 +0200

    created sql script for init trip_status

---
+++ sql_scripts/insert_default_tripStatus.sql
@@ -0,0 +1,6 @@
+INSERT INTO trip_status(id, name) VALUES (1, "En création");
+INSERT INTO trip_status(id, name) VALUES (2, "Ouvert");
+INSERT INTO trip_status(id, name) VALUES (3, "Pleine");
+INSERT INTO trip_status(id, name) VALUES (4, "Fermée");
+INSERT INTO trip_status(id, name) VALUES (5, "Finie");
+INSERT INTO trip_status(id, name) VALUES (6, "Anulée");
