9cb2c747e8f409503e86dce980476bcd9b4dcae3
Yoann P <yoann.pommier777@gmail.com>
2019-10-24 15:26:10 +0200

    finished trip table table

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,27 @@
+function getUserImage(username, background = [255, 255, 255, 0], colored) {
+    const hash = sha1(username);
+    let col = [0, 0, 0, 255];
+
+    if (colored) {
+        for(let i = 0; i<3; i++) {
+            let hashBuffer = hash.slice(i*12, (i*12)+12);
+            let stringCode = "";
+            for(let j = 0; j < hashBuffer.length; j++) {
+                stringCode += hashBuffer.charCodeAt(j);
+            }
+            col[i] = Number(stringCode) % 255;
+        }
+    }
+
+    const img = new Identicon( hash, {
+        foreground: col,
+        background: background,
+        size: 420,
+        format: 'png'
+    });
+    return `data:image/png;base64,${img}`;
+}
+
 $(document).ready(function() {
     // fix menu when passed
     $('.masthead').visibility({
