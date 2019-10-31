276a1850bfda38ff726001e0f60114a679ad1b1e
ThomasRODRIGUEZ <thomas.rodriguez2018@campus-eni.fr>
2019-10-31 15:11:11 +0100

    ajout de 2 sorties pour la démo, test => OK

--- demo/sql_scripts/default_trip.sql
+++ demo/sql_scripts/default_trip.sql
@@ -1,3 +1,6 @@
 INSERT INTO `trip` (`id`, `participant_area_id`, `status_id`, `place_id`, `organizer_id`, `name`, `start_date`, `duration`, `deadline_date`, `max_registration_number`, `description`) 
-VALUES (NULL, '1', '2', '3', '1', 'Halloween', '2019-10-31 22:15:00', '120', '2019-10-31 18:20:00', '15', 'Ouais Halloween trop bien !'), 
-(NULL, '1', '2', '3', '1', 'Noel', '2019-12-25 00:00:01', '1440', '2019-12-20 18:00:00', '30', 'Vive le vent d\'hiver !')
+VALUES (NULL, '1', '2', '1', '1', 'Halloween', '2019-10-31 22:15:00', '120', '2019-10-31 18:20:00', '15', 'Ouais Halloween trop bien !'), 
+(NULL, '1', '2', '2', '1', 'Noel', '2019-12-25 00:00:01', '1440', '2019-12-20 18:00:00', '30', 'Vive le vent d\'hiver !'),
+(NULL, '1', '2', '2', '1', 'Sortie à la patinoire', '2019-11-05 20:45:00', '120', '2019-11-04 20:45:00', '8', 'Petit soirée entre camarade de classe!'), 
+(NULL, '1', '1', '3', '1', 'Nouvel an au Meltdown', '2019-12-31 23:00:00', '120', '2019-12-21 23:00:00', '40', '3..2..1..BONNE ANNEE!')
+
